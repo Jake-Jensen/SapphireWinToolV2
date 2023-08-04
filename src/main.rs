40d435626df9e99af80e9772e27647bf08e16e08
Jake Jensen <jakejensenxk@gmail.com>
2023-08-04 10:10:34 -0400

    Added super barebones startup code

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@
 #![allow(non_snake_case)]
 
 fn main() {
+    if !is_elevated::is_elevated() {
+        let _ = msgbox::create("Warning", 
+            "This program requires administrative rights to perform most actions. I will automatically close after this box is closed.", 
+            msgbox::IconType::Info);
+    } else {
+        println!("------ Sapphire Win Tool V2 - Rusty ------")
+    }
+    // Continue exection.
+
     
 }
